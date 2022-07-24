fb940cf43a90f7fdaf17b47f3a49627b56d7f6de
blairyeung <53135008+blairyeung@users.noreply.github.com>
2022-07-24 02:44:02 -0400

    Regression Analaysis

--- R code/Market_share.R
+++ R code/Market_share.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(ggrepel)
 library(tidyverse)
+library(ggsci)
 
 blank_theme <- theme_minimal()+
   theme(
@@ -12,12 +13,14 @@ blank_theme <- theme_minimal()+
     plot.title=element_text(size=14, face="bold")
   )
 
-my_colors = c('#00549E', #009FC2', '#936FB1', '#850C70', '#B30838', '#EE2E24', '#FFAD59', '#E0C000', '#91BDA6', '#6CB33F', '#00703C')
+my_colors = c('#00549E', '#009FC2', '#936FB1', '#850C70', '#B30838', '#EE2E24', '#FFAD59', '#E0C000', '#91BDA6', '#6CB33F', '#00703C')
+less_colors = c('#009FC2', '#936FB1' , '#D9839D' ,'#FFAD59', '#91BDA6', '#6CB33F')
+              
 m_share <- read.csv('D:/Github/KPMG-Contest/Tables/mobile_game_market_share.csv')
 
 colnames(m_share) <- c('Game_type', 'Market_share', 'Order')
 
-m_plot
+
 
 
 m_share2 <- m_share %>% 
@@ -25,11 +28,12 @@ m_share2 <- m_share %>%
          pos = Market_share/2 + lead(csum, 1),
          pos = if_else(is.na(pos), Market_share/2, pos))
 
-ggplot(m_share2, aes(x = "" , y = Market_share, fill = fct_inorder(Game_type))) +
+m_plot <- ggplot(m_share2, aes(x = "" , y = Market_share, fill = fct_inorder(Game_type))) +
   geom_col(width = 1, color = 1) +
   coord_polar(theta = "y") +
   geom_text(aes(label = Market_share),
-            position = position_stack(vjust = 0.5)) +
-  scale_fill_brewer(palette="my_colors") +
+            position = position_stack(vjust = 0.5) , color="white") +
+  scale_fill_nejm() +
   blank_theme
 
+m_plot
---
+++ R code/gamma.R
@@ -0,0 +1,141 @@
+
+# Raw Data INPUT
+df <- read.csv('D:/Github/KPMG-Contest/Tables/Raw_wzry.csv')
+
+colnames(df) <- c('age', 'value')
+
+i = 22
+
+fi = 0
+
+var_1 = 0.01
+# var_2 = 0.1001
+
+mean_age = c(17, 25, 35, 45, 55)
+const = 500
+
+sqrtpi = sqrt(2 * 3.14159)
+
+e = 2.71828
+
+fi = 0
+
+for (ind in 1:nrow(df)){
+  print(i)
+  data = df$value[ind]
+  print(data)
+  diff = (i - mean_age[ind]) / const
+  add =  data * (e^( - (diff^2)/ (2 * (var_1^2) ) ) )/ (var_1 * sqrtpi)
+  substract = data * (e^( - (diff^2)/ (2 * (var_2^2) ) ) )/ (var_2 * sqrtpi)
+  fi = fi + add - substract
+  # print(diff)
+  # print(add)
+  # print(diff^2)
+  # print(((diff^2)/ (2 * (var_2^2) ) ))
+}
+
+
+print(fi)
+
+
+print (df)
+
+vect <- c(1:55)
+
+for (v in c(1:12)){
+  vect[v] = 0
+}
+
+median_age  = c(17, 23, 31, 42, 60)
+
+
+for (i in c(5:55)){
+  fi = 0
+  
+  for (ind in 1:nrow(df)){
+    data <- df$value[ind]
+    # data <- 1
+    # print(data)
+    diff = (i - median_age[ind]) / const
+    # beta = i^0.2
+    beta = 1
+    add = (beta * e^( - (diff^2)/ (2 * (var_1^2) ) ) )/ (var_1 * sqrtpi) * data
+    substract = (e^( - (diff^2)/ (2 * (var_2^2) ) ) )/ (var_2 * sqrtpi) * data
+    substract = 0
+    fi = fi + add - substract
+    # print(diff)
+    # print(add)
+  }
+  
+  vect[i] = fi
+  print(fi)
+}
+
+expceted_data <- data.frame(age = c(1:55),
+                            val = vect
+)
+
+raw_data <- data.frame(age = mean_age, 
+                       val = df$value)
+
+# Compare raw
+
+unnormalized <- c(0,0,0,0,0)
+
+
+median_age  = c(17, 25, 35, 45, 55)
+
+for (k in c(1:4)){
+  start = median_age[k]
+  end = median_age[k+1]
+  print(start)
+  print(end)
+  unnormalized[k+1] =  sum(vect[start:end])
+}
+
+unnormalized[1] = sum(vect[1:17])
+
+print(unnormalized)
+
+
+unnormalized_data <- data.frame(age = mean_age,
+                                val = unnormalized
+)
+
+norm_fact <- sum(unnormalized)
+
+normalized <- c(1:5)
+
+for (i in c(1:5)){
+  normalized[i] = unnormalized[i] / norm_fact
+}
+
+
+normalized_data <- data.frame(age = mean_age,
+                              val = normalized
+)
+print(normalized)
+
+
+# Augmented data plot
+
+ggplot() +
+  geom_line(data = expceted_data, aes(x = age, y = val))
+
+
+# Unaugmented data plot
+
+ggplot() +
+  geom_line(data = normalized_data, aes(x = age, y = val*100)) +
+  geom_line(data = raw_data, aes(x = age, y = val, color = 'red'))
+
+compare_data <- data.frame(actual = df$value,
+                              expected = normalized
+)
+
+ggplot(compare_data, aes(x = actual, y = expected)) + 
+  geom_point() +
+  stat_summary(fun.compare_data= mean_cl_normal) + 
+  geom_smooth(method='lm')
+
+
